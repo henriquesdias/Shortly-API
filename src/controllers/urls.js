a95ac8aa144cdd3b8e79047dd9eb086c9ad25f18
henriquesdias <henrique.dias1915@gmail.com>
2022-10-12 19:34:20 -0300

    feat: get 'shortUrl' by id. If this 'shortUrl' do not exist, status code 404 is send

--- src/controllers/urls.js
+++ src/controllers/urls.js
@@ -18,5 +18,20 @@ async function postUrl(req, res) {
     res.status(500).send(error.message);
   }
 }
+async function getUrlById(req, res) {
+  const { id } = req.params;
+  try {
+    const url = await connection.query(
+      'SELECT id, "shortUrl", url  FROM urls WHERE id = $1',
+      [id]
+    );
+    if (url.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+    res.status(200).send(url.rows[0]);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
-export { postUrl };
+export { postUrl, getUrlById };
--- src/routes/urls.js
+++ src/routes/urls.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { validateUrl } from "../middlewares/urls-schema.js";
-import { postUrl } from "../controllers/urls.js";
+import { postUrl, getUrlById } from "../controllers/urls.js";
 import { validateTokenFromUser } from "../middlewares/validate-token.js";
 
 const router = express.Router();
 router.post("/urls/shorten", validateTokenFromUser, validateUrl, postUrl);
+router.get("/urls/:id", getUrlById);
 
 export default router;
